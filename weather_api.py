911d5cc2ebfa797091470329f83d5da627efbb44
Yuriy Y <monkcrazy91@gmail.com>
2023-08-16 02:00:27 +0600

    Add function to get dictionary with weather indicators

--- weather_api.py
+++ weather_api.py
@@ -2,8 +2,9 @@ import os
 import requests
 
 from dotenv import load_dotenv
-from typing import Any
+from typing import Any, Dict
 from geopy.geocoders import Nominatim
+from datetime import datetime
 
 load_dotenv(".env")
 API_KEY = os.getenv("API_KEY")
@@ -47,3 +48,32 @@ def get_five_day_weather_json(city_name):
         f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&&units=metric&appid={API_KEY}&lang=ru"
     )
     return weather_data.json()
+
+
+def get_current_weather(city_name: str) -> Dict:
+    """Return a dictionary with weather indicators"""
+    city_data = get_today_weather_json(city_name)
+
+    temp = city_data["main"]["temp"]
+    weather = city_data["weather"][0]["description"]
+    humidity = city_data["main"]["humidity"]
+    wind_speed = city_data["wind"]["speed"]
+    pressure = city_data["main"]["pressure"]
+    timestamp_sunrise = city_data["sys"]["sunrise"]
+    sunrise = datetime.fromtimestamp(timestamp_sunrise)
+    timestamp_sunset = city_data["sys"]["sunset"]
+    sunset = datetime.fromtimestamp(timestamp_sunset)
+    day_length = sunset - sunrise
+    hours, _ = divmod(day_length.seconds, 3600)
+
+    weather = {
+        "Температура": f"{temp} °C",
+        "Тип погоды": f"{weather}",
+        "Влажность": f"{humidity}",
+        "Скорость ветра": f"{wind_speed} м/с",
+        "Атмосферное давление": f"{pressure}",
+        "Восход": f"{sunrise}",
+        "Закат": f"{sunset}",
+        "Продолжительность дня": f"{hours} часов",
+    }
+    return weather
