b20762381b9ed0c99e885eb1e2ffdd72120f72b8
Yuriy Y <monkcrazy91@gmail.com>
2023-08-16 22:47:15 +0600

    Change asyncio to executor

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import asyncio
 import logging
 import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
+from aiogram.utils import executor
 
 from handlers.weather_handlers import register_handlers
 
@@ -16,7 +16,7 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-async def main():
+async def main(dp: Dispatcher):
     """Start the bot and handlers"""
     register_handlers(dp)
     await dp.start_polling(bot)
@@ -24,4 +24,4 @@ async def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True, on_startup=main)
